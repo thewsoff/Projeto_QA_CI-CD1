0d95ba8223e633bf2b405aa1cf5442a3e1dfa71c
Matheus <159590811+thewsoff@users.noreply.github.com>
2025-04-16 21:00:31 -0300

    Update test_math-utils.py

--- test_math-utils.py
+++ test_math-utils.py
@@ -3,3 +3,12 @@ def test_add():
     assert add(2, 3) == 5
 def test_subtract():
     assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+def test_multiply_invalid_type():
+    try:
+        multiply("a", 3)
+    except ValueError:
+        assert True
